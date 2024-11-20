5fd19c392fbb3e24959c8521f1b6709ae4a4f6bc
Pragya Mishra <136903679+mishrashivi1@users.noreply.github.com>
2024-11-20 16:06:30 +0000

    container config done

---
+++ basics/variableandconstant.js
@@ -0,0 +1,11 @@
+const account=1222
+// not allowed to change
+let accountemail="pragyamishra244@gmail.com"
+var accountpass="123456"//not used because of scope
+accountcity="kanpur"
+let accountstate;
+console.log(account);
+accountemail="pm"
+accountpass="34"
+accountcity="think"
+console.table([account,accountemail,accountpass,accountcity,accountstate])
