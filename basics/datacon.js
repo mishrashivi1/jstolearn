3876352c9d24e99763c53a323d95873e4ea5e38e
Pragya Mishra <136903679+mishrashivi1@users.noreply.github.com>
2024-11-20 16:44:11 +0000

    convestion of data types

---
+++ basics/datacon.js
@@ -0,0 +1,32 @@
+let age="33";
+console.log(typeof(age));
+let age2=Number(age);
+console.log(typeof(age2));
+let a="123al"
+let age3=Number(a);
+console.log(typeof(age3));
+console.log(age3);
+let b=null
+let age4=Number(b);
+console.log(typeof(age4));
+console.log(age4);
+let c=undefined;
+let age5=Number(a);
+console.log(typeof(age5));
+console.log(age5);
+let d=true
+let age6=Number(d);
+console.log(typeof(age6));
+console.log(age6);
+let e="1al"
+let age7=Number(e);
+console.log(typeof(e));
+console.log(age7);
+//"33"=>33
+//"33abc" =>nan
+//boolean
+let g=1;
+let changeg=Boolean(g);
+console.log(changeg);
+//1 is true and 0 is false
+//"" is false and "w" is true
