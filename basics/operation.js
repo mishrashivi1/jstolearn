419452a91c0c9b57f354dab95de5d4d89faebf9d
Pragya Mishra <136903679+mishrashivi1@users.noreply.github.com>
2024-11-20 16:54:54 +0000

    operator and converstion

---
+++ basics/operation.js
@@ -0,0 +1,14 @@
+let a=9;
+let nega=-a;
+console.log(nega);
+console.log(2+2);
+console.log(2-2);
+console.log(2/2);
+console.log(2*2);
+console.log(2**2);
+console.log(2%2);
+//behavior of js
+console.log("1"+2);
+console.log(2+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");//32 due to code reaf from left to right
