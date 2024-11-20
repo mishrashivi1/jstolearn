94bbcccee2a8f4d5051fd1eae037eb3337bf7b56
Pragya Mishra <136903679+mishrashivi1@users.noreply.github.com>
2024-11-20 16:27:37 +0000

    datatypes

---
+++ basics/databtypes.js
@@ -0,0 +1,20 @@
+"use strict";
+// treated as new version
+//alert(3+3)
+//used in brower only on in ide
+console.log(3+3);
+let name="aa"//string data types
+let age=10//number=2^53 or for larger we use big int or integer
+let a=true;
+let b=null;
+let c;
+console.table(typeof "aa");
+console.table(typeof 10);
+console.table(typeof true);
+console.table(typeof null);
+console.table(typeof c);
+
+// boolean=true/false
+// null is standalone value
+// undefine not inputed
+//non premitive types is object
