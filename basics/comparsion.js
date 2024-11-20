65f34770ac4b7f9585e454e51a9bdaa399eac037
Pragya Mishra <136903679+mishrashivi1@users.noreply.github.com>
2024-11-20 17:06:19 +0000

    comparsion

---
+++ basics/comparsion.js
@@ -0,0 +1,25 @@
+console.log(2>1);
+console.log(2<1);
+console.log(2>=1);
+console.log(2<=1);
+console.log(2!=1);
+console.log(2==1);
+console.log("2">1);
+console.log("02">1);
+console.log(null>0);//false
+console.log(null==0);//false
+console.log(null>=0);//true due to different treat null=0
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);//false
+// strict checking (===) both datatype and value
+console.log(2===3);
+
+
+
+
+
+
+
+
+
