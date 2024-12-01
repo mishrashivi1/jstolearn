c009cdb9e11765556f2fea574a73c6e681b639f6
Pragya Mishra <136903679+mishrashivi1@users.noreply.github.com>
2024-12-01 08:29:20 +0000

    objects

---
+++ highorder/object.js
@@ -0,0 +1,13 @@
+const myobject={
+    js:"javascript",
+    cpp:"c++",
+    c:"c",
+    py:"python",
+    sql:"struture query language",
+}
+for (const key in myobject) {
+   console.log(`${key} of ${myobject[key]}`);
+   
+}
+//if we use for in  loop the keyb will print
+//map are initratable
