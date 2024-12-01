4d978e11cc0dc2d9fe20769478fce133de4c0662
Pragya Mishra <136903679+mishrashivi1@users.noreply.github.com>
2024-12-01 08:29:09 +0000

    map function

---
+++ highorder/map.js
@@ -0,0 +1,32 @@
+const nums=[1,2,3,4,5,6,7,8,9,10];
+//  const ans=nums.filter((items)=>items>=5)
+//  console.log(ans);
+// const ans=nums.filter((items)=>
+//     {
+//          return items>=5
+
+//     })
+// console.log(ans);
+// const newarray=[];
+// nums.forEach((num)=>
+// {
+//     if(num>4)
+//     {
+//         newarray.push(num);
+//     }
+// })
+// console.log(newarray);
+// const newarray=[];
+// nums.filter((num)=>
+// {
+//     if(num>4)
+//     {
+//         newarray.push(num);
+//     }
+// })
+// console.log(newarray);
+const value=nums.map((num)=>num+10);
+console.log(value);
+
+
+ 
